97c40fdf9ae8f1e2d1d1d14532df57540e71f031
pkieber <113725311+pkieber@users.noreply.github.com>
2023-10-07 22:17:40 +0200

    add getStarted + button-component

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testi
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className="">HELLO</div>
       <div className={`${styles.boxWidth}`}>
         <Navbar />
       </div>
     </div>
+
     <div className={`bg-primary ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
         <Hero />
